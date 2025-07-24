2281437f4b3828a54956550226b1ac13c40838af
Dickson Tan <dickson.tan.2013@gmail.com>
2025-07-24 13:39:51 +0800

    update GitLab script

--- gitlab a11y fixes.user.js
+++ gitlab a11y fixes.user.js
@@ -5,88 +5,157 @@
 // @copyright 2018
 // @license GNU General Public License version 2.0
 // @version        1.0
-// @require  https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require  https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require https://raw.githubusercontent.com/Neurrone/greasemonkey-scripts/master/waitForKeyElements.js
 // @grant GM_log
 // @include https://gitlab.com/*
 // ==/UserScript==
-/*
+
 // Give add comment buttons on difs a proper label
 function onDifButtonLoad(index, target) {
-    for (elem of target.querySelectorAll(":first-child"))
-        elem.setAttribute("aria-label", "Add Comment");
+  target.setAttribute("aria-label", "Add Comment");
+  target.style.cssText = ""; // strip display: none, making it invisible
+  target.classList.add("screen-reader");
+  target.classList.add("screen-reader-focusable");
 }
 
-// Give dif table cells  a role=cell
-function onDifTableCellLoad(index, target) {
-    target.setAttribute("role", "table");
-    for (elem of target.querySelectorAll("tr"))
-        elem.setAttribute("role", "row");
-    for (elem of target.querySelectorAll("td"))
-        elem.setAttribute("role", "cell");
-    for (elem of target.querySelectorAll("tr:last-child"))
-        elem.setAttribute("aria-hidden", "true");
+function onDifTableLoad(index, target) {
+  target.setAttribute("role", "table");
 }
-*/
+
+function onDifTableRowLoad(index, target) {
+  target.setAttribute("role", "row");
+}
+
+function onDifTableRowPresentationLoad(index, target) {
+  target.setAttribute("role", "presentation");
+}
+
+function onDifTableCellLoad(index, target) {
+  if (
+    target.className.indexOf("line-coverage") >= 0 ||
+    target.className.indexOf("line-codequality") >= 0
+  ) {
+    target.setAttribute("role", "presentation");
+  } else {
+    target.setAttribute("role", "cell");
+  }
+}
 
 /*
-Add table semantics to divs being used for the issues table, which have the following structure:
-<ul class="content-list issues-list issuable-list"> // set role="table"
-  <li class="issue" data-id="95" data-labels="[]" id="issue_95" url="/..."> // set role="row", each issue is inside a <li>
-    <div class="issue-box"> // set role="presentation"
-      <div class="issue-info-container"> // role="presentation"
-        <div class="issue-main-info"> // role = "presentation", children are actual cells
-          <div class="issue-title title">
-          <div class="issuable-info">
-        <div class="issuable-meta"> // role = "presentation", children are cells
-          <ul class="controls"> // also set child <li>s's role to presentational
-          <div class="float-right issuable-updated-at d-none d-sm-inline-block">
-
-The div structure for the merge request table is also virtually identical, so we handle it here.
-
-<ul class="content-list mr-list issuable-list">
-  <li class="merge-request" data-id="1600" data-labels="[...]" id="merge_request_1600"> // role="row", each MR is inside a <li>
-    <div class="issue-info-container"> // role = "presentation"
-      <div class="issue-main-info"> // role = "presentation", children are actual cells
-        <div class="merge-request-title title">
-        <div class="issuable-info">
-      <div class="issuable-meta"> 
-        <ul class="controls"> // also set child <li>s's role to presentational
-        <div class="float-right issuable-updated-at d-none d-sm-inline-block">
-
-also, fix the number of related merge requests, comments and upvotes being shown on its own with no accessible indication of what the numbers mean
-*/
-
+  Add table semantics to divs being used for the issues table, which have the following structure:
+  <ul class="content-list issues-list issuable-list"> // set role="table"
+    <li class="issue" data-id="95" data-labels="[]" id="issue_95" url="/..."> // set role="row", each issue is inside a <li>
+      <div class="issue-box"> // set role="presentation"
+        <div class="issue-info-container|issuable-info-container"> // role = "presentation", newer versions of gitlab use issuable-info-container for the class name
+          <div class="issue-main-info|issuable-main-info"> // role = "presentation", children are actual cells
+            <div class="issue-title title">
+            <div class="issuable-info">
+          <div class="issuable-meta"> // role = "presentation", children are cells
+            <ul class="controls"> // also set child <li>s's role to presentational
+            <div class="float-right issuable-updated-at d-none d-sm-inline-block">
+  
+  The div structure for the merge request table is also virtually identical, so we handle it here.
+  
+  <ul class="content-list mr-list issuable-list"> // role="table"
+    <li class="merge-request" data-id="1600" data-labels="[...]" id="merge_request_1600"> // role="row", each MR is inside a <li>
+      <div class="issue-info-container|issuable-info-container"> // role = "presentation", newer versions of gitlab use issuable-info-container for the class name
+        <div class="issue-main-info|issuable-main-info"> // role = "presentation", children are actual cells
+          <div class="merge-request-title title"> // role = "cell"
+          <div class="issuable-info"> // role="cell"
+        <div class="issuable-meta"> // role="presentation"
+          <ul class="controls"> // role="cell", set child <li>s's role to presentational
+          <div class="float-right issuable-updated-at d-none d-sm-inline-block"> //role="cell"
+  
+  also, fix the number of related merge requests, comments and upvotes being shown on its own with no accessible indication of what the numbers mean
+  */
 function onIssuesOrMRTableLoad(index, target) {
-    target.setAttribute("role", "table");
-    for (row of target.querySelectorAll("li.issue, li.merge-request"))
-        row.setAttribute("role", "row");
-    for (elem of target.querySelectorAll("div.issue-box, div.issue-info-container, div.issue-main-info, div.issuable-meta, ul.controls > *"))
-        elem.setAttribute("role", "presentation");
-    for (cell of target.querySelectorAll('.issue-main-info > div, .issuable-meta > *'))
-        cell.setAttribute("role", "cell");
-	for (elem of target.querySelectorAll("li.issuable-mr"))
-		elem.innerHTML = elem.innerText + " related MRs";
-	for (elem of target.querySelectorAll("li.issuable-comments"))
-		elem.innerHTML = elem.innerText + " comments";
-	for (elem of target.querySelectorAll("li.issuable-upvotes"))
-		elem.innerHTML = elem.innerText + " upvotes";
+  target.setAttribute("role", "table");
+  for (row of target.querySelectorAll("li.issue, li.merge-request"))
+    row.setAttribute("role", "row");
+  for (elem of target.querySelectorAll(
+    "div.issue-box, div.issue-info-container, div.issuable-info-container, div.issue-main-info, div.issuable-main-info, div.issuable-meta, ul.controls > *"
+  ))
+    elem.setAttribute("role", "presentation");
+  for (cell of target.querySelectorAll(
+    ".issue-check, .issue-main-info > div, .issuable-main-info > div, .issuable-meta > *"
+  ))
+    cell.setAttribute("role", "cell");
+  for (elem of target.querySelectorAll("li.issuable-mr"))
+    elem.innerHTML = elem.innerText + " related MRs";
+  for (elem of target.querySelectorAll("li.issuable-comments"))
+    elem.innerHTML = elem.innerText + " comments";
+  for (elem of target.querySelectorAll("li.issuable-upvotes"))
+    elem.innerHTML = elem.innerText + " upvotes";
 }
 
-function onDiffLoad (index, target) {
-	for (elem of target.querySelectorAll("div.file-header-content > a")) {
-		elem.setAttribute("role", "heading");
-		elem.setAttribute("aria-level", "4");
-	}
-	
-	for (elem of target.querySelectorAll("a.js-toggle-diff-comments"))
-		elem.setAttribute("aria-label", "toggle comments for this file");
+// Make file titles headings for faster navigation
+function onDiffLoad(index, target) {
+  for (elem of target.querySelectorAll("strong.file-title-name")) {
+    elem.setAttribute("role", "heading");
+    elem.setAttribute("aria-level", "4");
+  }
 }
+
+var sheet = document.createElement("style");
+sheet.innerHTML = `
+  /**
+   * see https://gomakethings.com/hidden-content-for-better-a11y/#hiding-the-link
+   * Visually hide an element, but leave it available for screen readers
+   * @link https://github.com/h5bp/html5-boilerplate/blob/master/dist/css/main.css
+   * @link http://snook.ca/archives/html_and_css/hiding-content-for-accessibility
+   */
+  .screen-reader {
+      border: 0;
+      clip: rect(0 0 0 0);
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      white-space: nowrap;
+      width: 1px;
+  }
+  
+  /**
+   * Extends the .screen-reader class to allow the element to be focusable when navigated to via the keyboard
+   * @link https://github.com/h5bp/html5-boilerplate/blob/master/dist/css/main.css
+   * @link https://www.drupal.org/node/897638
+   */
+  .screen-reader-focusable:active,
+  .screen-reader-focusable:focus {
+      clip: auto;
+      height: auto;
+      margin: 0;
+      overflow: visible;
+      position: static;
+      white-space: normal;
+      width: auto;
+  }
+  `;
+document.body.appendChild(sheet);
 
 // wait for ajax calls to load the required elements
-//waitForKeyElements ('[title="Add a comment to this line"]', function(nodes) { nodes.each(onDifButtonLoad); });
-//waitForKeyElements ('[table, .diff-wrap-lines', function(nodes) { nodes.each(onDifTableCellLoad); });
+waitForKeyElements(".add-diff-note", function (nodes) {
+  nodes.each(onDifButtonLoad);
+});
+waitForKeyElements("div.diff-wrap-lines", function (nodes) {
+  nodes.each(onDifTableLoad);
+});
+waitForKeyElements("div.diff-grid-row", function (nodes) {
+  nodes.each(onDifTableRowLoad);
+});
+waitForKeyElements("div.diff-grid-row > div", function (nodes) {
+  nodes.each(onDifTableRowPresentationLoad);
+});
+waitForKeyElements("div.diff-grid-row > div > div", function (nodes) {
+  nodes.each(onDifTableCellLoad);
+});
 
-waitForKeyElements ('ul.issuable-list', function(nodes) { nodes.each(onIssuesOrMRTableLoad); });
-waitForKeyElements ('div.file-holder', function(nodes) { nodes.each(onDiffLoad); });
+waitForKeyElements("ul.issuable-list", function (nodes) {
+  nodes.each(onIssuesOrMRTableLoad);
+});
+waitForKeyElements("div.file-header-content", function (nodes) {
+  nodes.each(onDiffLoad);
+})
